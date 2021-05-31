4e35d504b8d8cb5daca9fc592a4e66167cf52ac6
Shivamk1612 <53030306+Shivamk1612@users.noreply.github.com>
2021-05-31 12:53:29 +0530

    pystan added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from datetime import date
 import yfinance as yf
+import pystan
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
